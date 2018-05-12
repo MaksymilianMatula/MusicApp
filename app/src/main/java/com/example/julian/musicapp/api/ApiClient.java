7e55597092f0bf1320bf2871cbf40608f21f7fe7
MaksymilianMatuła <w57091@student.wsiz.rzeszow.pl>
2018-05-12 15:17:33 +0200

    retrofit2

--- app/src/main/java/com/example/julian/musicapp/api/ApiClient.java
+++ app/src/main/java/com/example/julian/musicapp/api/ApiClient.java
@@ -1,4 +1,11 @@
 package com.example.julian.musicapp.api;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiClient {
+    @GET( "trending.php" )
+    Call<TrendingList> getTrendingList(@Query( "country" ) String country, @Query ( "type" )
+            String type, @Query ( "format" ) String format);
 }
