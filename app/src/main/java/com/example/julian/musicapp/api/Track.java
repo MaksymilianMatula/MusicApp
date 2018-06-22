0c4bebfbb738df5dbc649ef0cb54ecea73d108f1
Maksymilian Matuła <w57091@student.wsiz.rzeszow.pl>
2018-06-22 15:30:36 +0200

    pobieranie danych

---
+++ app/src/main/java/com/example/julian/musicapp/api/Track.java
@@ -0,0 +1,10 @@
+package com.example.julian.musicapp.api;
+
+public class Track {
+    public String strAlbum;
+    public String strDescription;
+    public String strGenre;
+    public String strStyle;
+    public String strTrackThumb;
+
+}
--- app/src/main/java/com/example/julian/musicapp/topsongs/SongDetailsActivity.java
+++ app/src/main/java/com/example/julian/musicapp/topsongs/SongDetailsActivity.java
@@ -3,8 +3,15 @@ package com.example.julian.musicapp.topsongs;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.example.julian.musicapp.api.ApiService;
+import com.example.julian.musicapp.api.Tracks;
 import com.example.user.myapplication.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SongDetailsActivity extends AppCompatActivity {
 
@@ -17,6 +24,8 @@ public class SongDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_song_details);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         Intent intent = getIntent();
         String track = intent.getStringExtra(TRACK);
         String artist = intent.getStringExtra(ARTIST);
@@ -25,5 +34,16 @@ public class SongDetailsActivity extends AppCompatActivity {
         getSupportActionBar().setTitle(track);
         getSupportActionBar().setSubtitle(artist);
 
+        ApiService.getService().getTrack(trackId).enqueue(new Callback<Tracks>() {
+            @Override
+            public void onResponse(Call<Tracks> call, Response<Tracks> response) {
+                Toast.makeText(SongDetailsActivity.this, "Pobrano dane", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<Tracks> call, Throwable t) {
+                Toast.makeText(SongDetailsActivity.this, "Błąd pobierania danych: " + t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
